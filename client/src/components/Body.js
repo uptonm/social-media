b59a5645caaff32bd65bb40bb958f142f66f5ee4
Mike Upton <uptonm@wit.edu>
2019-01-03 14:47:41 -0500

    Fixed Body Page

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,18 +1,28 @@
-import React from "react";
-import { Segment, Statistic } from "semantic-ui-react";
-import paragraph from "../assets/paragraph.png";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Segment, Statistic } from 'semantic-ui-react';
+import paragraph from '../assets/paragraph.png';
 
-export default () => {
-  return (
-    <div className="half-width" align="center">
-      <Segment.Group>
-        <Segment vertical>
-          <Statistic label="Total Users" value="5,550" />
-        </Segment>
-        <Segment vertical>
-          <img src={paragraph} alt="paragraph" />
-        </Segment>
-      </Segment.Group>
-    </div>
-  );
-};
+class Body extends Component {
+  state = { users: 0 };
+  componentDidMount = async () => {
+    const users = await axios.get('http://localhost:8000/api/users');
+    this.setState({ users: users.data.length });
+  };
+  render() {
+    return (
+      <div className="half-width" align="center">
+        <Segment.Group>
+          <Segment vertical>
+            <Statistic label="Total Users" value={this.state.users} />
+          </Segment>
+          <Segment vertical>
+            <img src={paragraph} alt="paragraph" />
+          </Segment>
+        </Segment.Group>
+      </div>
+    );
+  }
+}
+
+export default Body;
